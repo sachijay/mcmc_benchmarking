79d13f660ef65616a72ef161a9ebabe3cef6f2a8
Pramoda Sachinthana Jayasinghe <30071227+sachijay@users.noreply.github.com>
2021-04-21 02:13:07 -0700

    Added library loads and figure directory

--- src/main.R
+++ src/main.R
@@ -1,5 +1,16 @@
+## Installs missing packages
+source(here::here("src", "install_packages.R"))
+
+## Load libraries
+library(ggplot2)
+
+
 ## Number of samples obtained by default
 n_samples <- 10000
 
 ## Burn-in proportion
 burn_prop <- 0.5
+
+## Figures directory
+fig_dir <- paste0(here::here("figures"),
+                  "/")
