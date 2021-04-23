15893a69400f1f3b84f7fe9eaab08ce5b93bca31
Pramoda Sachinthana Jayasinghe <30071227+sachijay@users.noreply.github.com>
2021-04-22 22:32:49 -0700

    Added PT to benchmarking

--- src/beta_binomial.R
+++ src/beta_binomial.R
@@ -1,3 +1,8 @@
+##############################################################
+## This file runs simulations for the Beta-Binomial problem ##
+##############################################################
+
+#####
 ## Run functions.R
 source(here::here("src", "functions.R"))
 
@@ -35,9 +40,9 @@ likelihood <- function(data, theta){
 
 
 ## Joint distribution
-joint_distribution <- function(data, theta, hyper_params){
+joint_distribution <- function(data, theta, hyper_params, annealing = 1){
   
-  out <- prior(theta, hyper_params) * likelihood(data, theta)
+  out <- prior(theta, hyper_params) * likelihood(data, theta)^annealing
   
   return(out)
 }
@@ -46,22 +51,43 @@ joint_distribution <- function(data, theta, hyper_params){
 ## Run benchmark for a single set of hyper parameters
 beta_bin_bench <- function(hyper_params, data, prior_len_out = 1000){
   
+  ## Run the MH algorithm
   begin_time <- Sys.time()
   mh_sample <- run_mh(data, 
                       n_samples = n_samples,
                       hyper_params = hyper_params,
+                      init_vals = 0.5,
                       sd = 1)
   end_time <- Sys.time()
   
-  ## Calculate the run time for the MCMC algorithm
-  run_time <- as.numeric(end_time - begin_time,
-                         units = "secs")
+  ## Calculate the run time for the MH algorithm
+  mh_run_time <- as.numeric(end_time - begin_time,
+                            units = "secs")
   
   ## Calculate posterior mean and MC SE
-  mcerr <- mcmcse::mcse(mh_sample)
+  mh_mcerr <- mcmcse::mcse(mh_sample)
   
   ## Calculate the effective sample size
-  ess <- mcmcse::ess(mh_sample)
+  mh_ess <- mcmcse::ess(mh_sample)
+  
+  begin_time <- Sys.time()
+  pt_sample <- run_pt(data,
+                      n_samples = n_samples,
+                      n_chains = 5,
+                      hyper_params = hyper_params,
+                      init_vals = 0.5,
+                      sd = 1)
+  end_time <- Sys.time()
+  
+  ## Calculate the run time for the PT algorithm
+  pt_run_time <- as.numeric(end_time - begin_time,
+                            units = "secs")
+  
+  ## Calculate posterior mean and MC SE
+  pt_mcerr <- mcmcse::mcse(pt_sample)
+  
+  ## Calculate the effective sample size
+  pt_ess <- mcmcse::ess(pt_sample)
   
   ## Get the prior dist
   prior_x <- seq(0, 1, length.out = prior_len_out)
@@ -70,21 +96,27 @@ beta_bin_bench <- function(hyper_params, data, prior_len_out = 1000){
   
   
   out <- list(param = hyper_params,
-              run_time = run_time,
-              ess = ess,
-              ess_per_time = ess/run_time,
+              mh_run_time = mh_run_time,
+              mh_ess = mh_ess,
+              mh_ess_per_time = mh_ess/mh_run_time,
+              mh_sample = mh_sample,
+              mh_pos_mean = mh_mcerr$est,
+              mh_mcse = mh_mcerr$se,
+              pt_run_time = pt_run_time,
+              pt_ess = pt_ess,
+              pt_ess_per_time = pt_ess/pt_run_time,
+              pt_sample = pt_sample,
+              pt_pos_mean = pt_mcerr$est,
+              pt_mcse = pt_mcerr$se,
               prior_x = prior_x,
-              prior = prior_density,
-              pos_sample = mh_sample,
-              pos_mean = mcerr$est,
-              mcse = mcerr$se)
+              prior = prior_density)
   
   return(out)
 }
 
 
 #####
-set.seed(520)
+set.seed(2021)
 
 ## Data fixed to be all failures
 data <- c(0, 0, 0)
@@ -94,39 +126,52 @@ data <- c(0, 0, 0)
 param_vec <- seq(1, 100, by = 2)
 
 ## Run the benchmark for all the parameters in the `param_vec`
-beta_bin_bench_vals <- lapply(param_vec, function(param, data){
-  
-  out <- beta_bin_bench(hyper_params = c(alpha = param, 
-                                         beta = param),
-                        data = data)
-  
-  return(out)
-  
-}, data = data)
+# beta_bin_bench_vals <- lapply(param_vec, function(param, data){
+#   
+#   out <- beta_bin_bench(hyper_params = c(alpha = param, 
+#                                          beta = param),
+#                         data = data)
+#   
+#   return(out)
+#   
+# }, data = data)
 
+## Save and load the benchmark for convenience
+beta_bin_data_file <- paste0(data_dir,
+                             "beta_bin_bench_vals.Rdata")
+# save(beta_bin_bench_vals,
+#      file = beta_bin_data_file)
+load(file = beta_bin_data_file)
+rm(beta_bin_data_file)
+
+
+
+####################################
+## Visualization of MH benchmarks ##
+####################################
 
 #####
 ## Extract ESS
-ess <- sapply(beta_bin_bench_vals, function(x){
-  return(x$ess)
+mh_ess <- sapply(beta_bin_bench_vals, function(x){
+  return(x$mh_ess)
 })
 
 ## Extract running time
-run_time <- sapply(beta_bin_bench_vals, function(x){
-  return(x$run_time)
+mh_run_time <- sapply(beta_bin_bench_vals, function(x){
+  return(x$mh_run_time)
 })
 
 ## Extract ESS per time
-ess_per_time <- sapply(beta_bin_bench_vals, function(x){
-  x$ess_per_time
+mh_ess_per_time <- sapply(beta_bin_bench_vals, function(x){
+  x$mh_ess_per_time
 })
 
 
 ## Plot the running time vs parameter value
 pdf(file = paste0(fig_dir,
-                  "beta_bin_time_params.pdf"))
+                  "beta_bin_mh_time_params.pdf"))
 ggplot(mapping = aes(x = param_vec,
-                     y = run_time)) + 
+                     y = mh_run_time)) + 
   geom_line() +
   theme_minimal() + 
   labs(x = latex2exp::TeX(r"($\alpha = \beta$)"),
@@ -137,7 +182,7 @@ dev.off()
 
 ## Plot ESS vs parameter value
 ggplot(mapping = aes(x = param_vec,
-                     y = ess)) + 
+                     y = mh_ess)) + 
   geom_line() +
   theme_minimal() + 
   labs(x = latex2exp::TeX(r"($\alpha = \beta$)"),
@@ -147,9 +192,9 @@ ggplot(mapping = aes(x = param_vec,
 
 ## Plot the ESS/time vs parameter value
 pdf(file = paste0(fig_dir,
-                  "beta_bin_esspt_params.pdf"))
+                  "beta_bin_mh_esspt_params.pdf"))
 ggplot(mapping = aes(x = param_vec,
-                     y = log(ess_per_time))) + 
+                     y = log(mh_ess_per_time))) + 
   geom_line() +
   theme_minimal() + 
   labs(x = latex2exp::TeX(r"($\alpha = \beta$)"),
@@ -170,10 +215,9 @@ y_theoretical <- dbeta(x,
 
 y_prior <- s1$prior
 
-
 pdf(file = paste0(fig_dir,
-                  "beta_bin_s1_dist.pdf"))
-ggplot(mapping = aes(x = s1$pos_sample,
+                  "beta_bin_mh_s1_dist.pdf"))
+ggplot(mapping = aes(x = s1$mh_sample,
                      y = after_stat(density))) + 
   geom_histogram() +
   geom_line(mapping = aes(x = x,
@@ -182,7 +226,8 @@ ggplot(mapping = aes(x = s1$pos_sample,
             size = 1) + 
   geom_line(mapping = aes(x = x,
                           y = y_prior,
-                          colour = "red")) + 
+                          colour = "red"), 
+            size = 1) + 
   scale_color_discrete(name = "Distribution", 
                        labels = c("Posterior", 
                                   "Prior")) +
@@ -191,6 +236,8 @@ ggplot(mapping = aes(x = s1$pos_sample,
        y = "Density", 
        title = latex2exp::TeX(sprintf(r"($\alpha = %d, \beta = %d$)", s1$param["alpha"], s1$param["beta"])))
 dev.off()
+
+rm(s1, x, y_theoretical, y_prior)
 
 s2 <- beta_bin_bench_vals[[length(beta_bin_bench_vals)]]
 x <- s2$prior_x
@@ -201,8 +248,8 @@ y_theoretical <- dbeta(x,
 y_prior <- s2$prior
 
 pdf(file = paste0(fig_dir,
-                  "beta_bin_s99_dist.pdf"))
-ggplot(mapping = aes(x = s2$pos_sample,
+                  "beta_bin_mh_s99_dist.pdf"))
+ggplot(mapping = aes(x = s2$mh_sample,
                      y = after_stat(density))) + 
   geom_histogram() +
   geom_histogram() +
@@ -221,3 +268,140 @@ ggplot(mapping = aes(x = s2$pos_sample,
        y = "Density",
        title = latex2exp::TeX(sprintf(r"($\alpha = %d, \beta = %d$)", s2$param["alpha"], s2$param["beta"])))
 dev.off()
+
+rm(s2, x, y_theoretical, y_prior)
+
+rm(mh_ess, mh_run_time, mh_ess_per_time)
+
+
+####################################
+## Visualization of PT benchmarks ##
+####################################
+
+#####
+## Extract ESS
+pt_ess <- sapply(beta_bin_bench_vals, function(x){
+  return(x$pt_ess)
+})
+
+## Extract running time
+pt_run_time <- sapply(beta_bin_bench_vals, function(x){
+  return(x$pt_run_time)
+})
+
+## Extract ESS per time
+pt_ess_per_time <- sapply(beta_bin_bench_vals, function(x){
+  x$pt_ess_per_time
+})
+
+
+## Plot the running time vs parameter value
+pdf(file = paste0(fig_dir,
+                  "beta_bin_pt_time_params.pdf"))
+ggplot(mapping = aes(x = param_vec,
+                     y = pt_run_time)) + 
+  geom_line() +
+  theme_minimal() + 
+  labs(x = latex2exp::TeX(r"($\alpha = \beta$)"),
+       y = "Run time (s)"
+       # title = "Runtime vs parameters for Beta-Binomial"
+  )
+dev.off()
+
+## Plot ESS vs parameter value
+ggplot(mapping = aes(x = param_vec,
+                     y = pt_ess)) + 
+  geom_line() +
+  theme_minimal() + 
+  labs(x = latex2exp::TeX(r"($\alpha = \beta$)"),
+       y = "ESS"
+       # title = "ESS vs parameters for Beta-Binomial"
+  )
+
+## Plot the ESS/time vs parameter value
+pdf(file = paste0(fig_dir,
+                  "beta_bin_pt_esspt_params.pdf"))
+ggplot(mapping = aes(x = param_vec,
+                     y = log(pt_ess_per_time))) + 
+  geom_line() +
+  theme_minimal() + 
+  labs(x = latex2exp::TeX(r"($\alpha = \beta$)"),
+       y = latex2exp::TeX(r"($\log_{10}$ (ESS per second) )")
+       # title = "ESS per sec vs parameters for Beta-Binomial"
+  )
+dev.off()
+
+
+#####
+
+## Plot end 2 posteriors and priors
+s1 <- beta_bin_bench_vals[[2]]
+x <- s1$prior_x
+y_theoretical <- dbeta(x, 
+                       shape1 = s1$param["alpha"] + sum(data), 
+                       shape2 = s1$param["beta"] + length(data) - sum(data))
+
+y_prior <- s1$prior
+
+pdf(file = paste0(fig_dir,
+                  "beta_bin_pt_s1_dist.pdf"))
+ggplot(mapping = aes(x = s1$pt_sample,
+                     y = after_stat(density))) + 
+  geom_histogram() +
+  geom_line(mapping = aes(x = x,
+                          y = y_theoretical,
+                          colour = "darkblue"), 
+            size = 1) + 
+  geom_line(mapping = aes(x = x,
+                          y = y_prior,
+                          colour = "red"), 
+            size = 1) + 
+  scale_color_discrete(name = "Distribution", 
+                       labels = c("Posterior", 
+                                  "Prior")) +
+  theme_minimal() + 
+  labs(x = "Posterior sample",
+       y = "Density", 
+       title = latex2exp::TeX(sprintf(r"($\alpha = %d, \beta = %d$)", s1$param["alpha"], s1$param["beta"])))
+dev.off()
+
+rm(s1, x, y_theoretical, y_prior)
+
+s2 <- beta_bin_bench_vals[[length(beta_bin_bench_vals)]]
+x <- s2$prior_x
+y_theoretical <- dbeta(x, 
+                       shape1 = s2$param["alpha"] + sum(data), 
+                       shape2 = s2$param["beta"] + length(data) - sum(data))
+
+y_prior <- s2$prior
+
+pdf(file = paste0(fig_dir,
+                  "beta_bin_pt_s99_dist.pdf"))
+ggplot(mapping = aes(x = s2$pt_sample,
+                     y = after_stat(density))) + 
+  geom_histogram() +
+  geom_histogram() +
+  geom_line(mapping = aes(x = x,
+                          y = y_theoretical,
+                          colour = "darkblue"), 
+            size = 1) + 
+  geom_line(mapping = aes(x = x,
+                          y = y_prior,
+                          colour = "red")) + 
+  scale_color_discrete(name = "Distribution", 
+                       labels = c("Posterior", 
+                                  "Prior")) +
+  theme_minimal() + 
+  labs(x = "Posterior sample",
+       y = "Density",
+       title = latex2exp::TeX(sprintf(r"($\alpha = %d, \beta = %d$)", s2$param["alpha"], s2$param["beta"])))
+dev.off()
+
+rm(s2, x, y_theoretical, y_prior)
+
+rm(pt_ess, pt_run_time, pt_ess_per_time)
+
+
+#####
+## Remove all variables from workspace
+rm(list = ls())
