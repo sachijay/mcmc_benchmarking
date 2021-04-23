90dc765c3a7293b81ba16ebab411ea2c6dee4a7c
Pramoda Sachinthana Jayasinghe <30071227+sachijay@users.noreply.github.com>
2021-04-22 22:31:15 -0700

    Added PT algorithm

--- src/functions.R
+++ src/functions.R
@@ -17,7 +17,7 @@ d_proposal <- function(x_s,
 r_proposal <- function(theta_i, 
                        sd = 1){
   
-  out <- rnorm(1, 
+  out <- rnorm(length(theta_i), 
                mean = theta_i,
                sd = sd)
   
@@ -29,48 +29,67 @@ r_proposal <- function(theta_i,
 ## Run the Metropolis-Hastings algorithm
 run_mh <- function(data, 
                    n_samples,
+                   init_vals,
                    hyper_params = NULL,
                    burn = TRUE,
-                   burn_prop = 0.5,
+                   burn_prop = 0.5, 
+                   annealing = 1,
                    ...){
   
+  n_unknown <- length(init_vals)
+  
   ## Initialize vector to save the sampled values
-  sampled_vals <- rep(NA, 
-                      times = n_samples)
+  sampled_vals <- matrix(NA, 
+                         nrow = n_samples+1,
+                         ncol = n_unknown)
   
   ## Initial parameter
-  sampled_vals[1] <- 0.5
+  sampled_vals[1,] <- init_vals
   
   
   for (j in 1:n_samples) {
     
-    x_i <- sampled_vals[j]
+    x_i <- sampled_vals[j,]
     
     x_s <- r_proposal(x_i, 
                       ...)
     
-    
     ## add the proposals if using a non-symmetric proposal
     # r <- joint_distribution(data, x_s, hyper_params) * d_proposal(x_i, x_s, ...) / 
     #   (joint_distribution(data, x_i, hyper_params) * d_proposal(x_s, x_i, ...))
-    r <- joint_distribution(data, x_s, hyper_params) / 
-      joint_distribution(data, x_i, hyper_params)
-    
+    r <- joint_distribution(data, x_s, hyper_params, annealing) / 
+      joint_distribution(data, x_i, hyper_params, annealing)
     
     accept <- rbinom(n = 1,
                      size = 1,
                      prob = min(1, r))
     
-    sampled_vals[j+1] <- ifelse(accept, 
-                                yes = x_s,
-                                no = x_i)
+    ## For debugging
+    # print("x_i = ")
+    # print(x_i)
+    # 
+    # print("x_s = ")
+    # print(x_s)
+    # 
+    # print("r = ")
+    # print(r)
+    # 
+    # print("accept = ")
+    # print(accept)
+    
+    
+    if(accept){
+      sampled_vals[j+1, ] <- x_s
+    } else {
+      sampled_vals[j+1, ] <- x_i
+    }
   }
   
   if(burn){
     ## Number burn-in
     n_burnin <- ceiling(burn_prop * n_samples)
     
-    out <- sampled_vals[(n_burnin+1):n_samples]
+    out <- sampled_vals[(n_burnin+1):n_samples, ]
   } else {
     out <- sampled_vals
   }
@@ -81,112 +100,94 @@ run_mh <- function(data,
 
 #####
 ## Run the parallel tempering algorithm
-run_pt <- function(data, 
+run_pt <- function(data,
                    n_samples,
                    n_chains,
+                   init_vals,
                    hyper_params = NULL,
                    burn = TRUE,
                    burn_prop = 0.5,
+                   sd = 1,
                    ...){
   
-  n_data <- length(data)
+  annealing_params <- seq(0, 1, length.out = n_chains)
   
-  b <- 0:n_data/n_data
+  ## Initialize vector to save the sampled values
+  sampled_vals <- matrix(NA,
+                         nrow = n_samples + 1,
+                         ncol = n_chains)
+  # sampled_vals <- rep(NA,
+  #                     times = n_samples + 1)
   
-  return(b)
+  ## Initial parameter
+  sampled_vals[1,] <- rep(init_vals, times = n_chains)
+  
+  
+  for (j in 1:n_samples) {
+    
+    proposal <- rep(NA, times = n_chains)
+    
+    for (ann in 1:n_chains) {
+      
+      ann_param <- annealing_params[ann]
+      
+      proposal[ann] <- head(run_mh(data,
+                                   n_samples = 1,
+                                   hyper_params = hyper_params,
+                                   init_vals = sampled_vals[j, ann],
+                                   annealing = ann_param,
+                                   sd = sd), 1)
+    }
+    
+    swap_chain <- sample(1:(n_chains - 1), size = 1)
+    
+    x_i <- proposal[swap_chain]
+    x_i1 <- proposal[swap_chain + 1]
+    
+    ann_i <- annealing_params[swap_chain]
+    ann_i1 <- annealing_params[swap_chain + 1]
+    
+    r <- joint_distribution(data, x_i1, hyper_params, ann_i) * joint_distribution(data, x_i, hyper_params, ann_i1) / 
+      joint_distribution(data, x_i, hyper_params, ann_i) * joint_distribution(data, x_i1, hyper_params, ann_i1)
+    
+    accept <- rbinom(n = 1,
+                     size = 1,
+                     prob = min(1, r))
+    
+    ## For debugging
+    # print("x_i = ")
+    # print(x_i)
+    # 
+    # print("x_s = ")
+    # print(x_s)
+    # 
+    # print("r = ")
+    # print(r)
+    # 
+    # print("accept = ")
+    # print(accept)
+    #
+    # print("swap_chain = ")
+    # print(swap_chain)
+    
+    
+    if(accept){
+      proposal[swap_chain] <- x_i1
+      proposal[swap_chain + 1] <- x_i
+    }
+    
+    sampled_vals[j+1, ] <- proposal
+    
+  }
+  
+  if(burn){
+    ## Number burn-in
+    n_burnin <- ceiling(burn_prop * n_samples)
+    
+    out <- sampled_vals[(n_burnin+1):n_samples, n_chains]
+  } else {
+    out <- sampled_vals[,n_chains]
+  }
+  
+  return(out)
 }
-
-run_pt(data, 
-       n_samples = n_samples,
-       n_chains = 10)
-
-
-#######################################################
-# temper<-function(niter, 
-#                  Bmin,
-#                  swap.interval,
-#                  rank,
-#                  size){
-#   
-#   swap = 0
-#   swaps.attempted = 0
-#   swaps.accepted = 0
-#   
-#   # Higher ranks run the higher "temperatures" (~smaller fractional powers)
-#   B = rep(0, size-1)
-#   
-#   for(r in 1:size-1){
-#     temp = (r-1)/(size-2)
-#     B[r] = Bmin^temp
-#   }
-#   
-#   
-#   # Create a list for proposal moves
-#   prop = rep(0,2);
-#   theta = matrix(0,niter,2)
-#   
-#   for(t in 2:niter){
-#     
-#     for(c in 1:length(prop))   
-#       prop = theta[t-1, c] + rnorm(1,0,0.1);
-#     
-#     #Calculate Log-Density at proposed and current position
-#     logdensity.current = logdensity(theta[t-1,])
-#     logdensity.prop = logdensity(prop);
-#     
-#     #Calculate log acceptance probability
-#     lalpha = B[rank]*(logdensity.prop-logdensity.current)
-#     
-#     if(log(runif(1))<lalpha){
-#       #Accept proposed move
-#       theta[t,] = prop;
-#       logdensity.current = logdensity.prop;
-#     }else{
-#       #Otherwise do not move
-#       theta[t,]=theta[t-1,];
-#     }
-#     
-#     if(t%%swap.interval ==0){
-#       for(evenodd in 0:1){
-#         swap=0;
-#         logdensity.partner=0;
-#         if(rank%%2 == evenodd%%2){
-#           rank.partner=rank + 1;
-#           #ranks range from 1:size-1. Cannot have a partner rank == size
-#           if(0<rank.partner && rank.partner<size){
-#             #On first iteration, evens receive from above odd
-#             #On second iteration, odds receive from above evens
-#             logdensity.partner<-mpi.recv.Robj(rank.partner,rank.partner);
-#             lalpha = (B[rank]-B[rank.partner])*(logdensity.partner-logdensity.current);
-#             swaps.attempted=swaps.attempted+1;
-#             if(log(runif(1))<lalpha){
-#               swap=1;
-#               swaps.accepted=swaps.accepted+1;
-#             }
-#             mpi.send.Robj(swap,dest=rank.partner,tag=rank)
-#           }
-#           if(swap==1){
-#             thetaswap=theta[t,];
-#             mpi.send.Robj(thetaswap,dest=rank.partner,tag=rank)
-#             theta[t,]=mpi.recv.Robj(rank.partner,rank.partner)
-#           }
-#         }else{
-#           rank.partner=rank-1;
-#           #ranks range from 1:size-1. Cannot have a partner rank ==0
-#           if(0<rank.partner && rank.partner<size){
-#             #On first iteration, odds send to evens below
-#             #On second iteration, evens sent to odds below
-#             mpi.send.Robj(logdensity.current,dest=rank.partner,tag=rank);
-#             swap=mpi.recv.Robj(rank.partner,rank.partner);
-#           }
-#           if(swap==1){
-#             thetaswap=theta[t,];
-#             theta[t,]=mpi.recv.Robj(rank.partner,rank.partner);
-#             mpi.send.Robj(thetaswap,dest=rank.partner,tag=rank);
-#           }
-#         }
-#       }
-#     }
-#   }
-#   return(theta)
-# }
